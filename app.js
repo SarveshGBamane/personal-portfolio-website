578c735af0b3cb16a3f83adfc83ab7551330ca03
Sarvesh Bamane <114917290+SarveshGBamane@users.noreply.github.com>
2025-01-24 23:37:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,22 +9,24 @@ let internshipBtn3 = document.getElementById("internshipBtn3");
 
 cvBtn.addEventListener("click", function(){
    alert("Download Succesfully")
-   window.open('image src/sarvesh_resume.jpg', '_blank');
+   window.open('https://drive.google.com/file/d/1vR7fwWnhvd6Pmi3I6dXR1-iFZZ4-H8zB/view?usp=drive_link', '_blank');
 })
+
+
 
 internshipBtn1.addEventListener("click", function(){
    alert("Download Succesfully")
-   window.open('image src/technohacks .jpg', '_blank');
+   window.open('https://drive.google.com/file/d/1vng_VABT-IgJ5jEZR6xSYSpRVTCpTVP3/view?usp=drive_link', '_blank');
 })
 
 internshipBtn2.addEventListener("click", function(){
    alert("Download Succesfully")
-   window.open('image src/codsoft.jpg', '_blank');
+   window.open('https://drive.google.com/file/d/1IeASFbex_YBaHMa1dOBywqIFkKIrQqri/view?usp=drive_link', '_blank');
 })
 
 internshipBtn3.addEventListener("click", function(){
    alert("Download Succesfully")
-   window.open('image src/zscalar.png', '_blank');
+   window.open('https://drive.google.com/file/d/11kkXhxQGr9CfDubRGpOPfF9clTEpiteF/view?usp=drive_link', '_blank');
 })
 
 
