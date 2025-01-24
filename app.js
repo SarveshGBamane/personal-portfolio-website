9b0f72b888fdab65c762628af9148ca0dff72939
Sarvesh Bamane <114917290+SarveshGBamane@users.noreply.github.com>
2025-01-24 22:06:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+let cvBtn = document.getElementById("cvBtn");
+
+
+let internshipBtn1 = document.getElementById("internshipBtn1");
+let internshipBtn2 = document.getElementById("internshipBtn2");
+let internshipBtn3 = document.getElementById("internshipBtn3");
+
+
+
+cvBtn.addEventListener("click", function(){
+   alert("Download Succesfully")
+   window.open('image src/sarvesh_resume.jpg', '_blank');
+})
+
+internshipBtn1.addEventListener("click", function(){
+   alert("Download Succesfully")
+   window.open('image src/technohacks .jpg', '_blank');
+})
+
+internshipBtn2.addEventListener("click", function(){
+   alert("Download Succesfully")
+   window.open('image src/codsoft.jpg', '_blank');
+})
+
+internshipBtn3.addEventListener("click", function(){
+   alert("Download Succesfully")
+   window.open('image src/zscalar.png', '_blank');
+})
+
+
